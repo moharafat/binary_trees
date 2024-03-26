f88618b4c8a47e9f833edc9740597463ede56e99
MariaGeorge22”git config --global user.email maria.george2222@gmail.comgit config --global user.name MariaGeorge22” <maria.george2222@gmail.com>
2024-03-26 13:05:05 -0700

    7-binary_tree_inorder.c

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,25 +1,17 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_inorder - binary tree function
+ * binary_tree_inorder - goes through a binary tree using in-order traversal
+ * @tree: pointer to the root node of the tree to traverse
+ * @func: pointer to a function to call for each node
  *
- * goes through a binary tree using in-order traversal
- * The value in the node must be passed as a parameter to this function.
- * If tree or func is NULL, do nothing
- *
- * @tree: a pointer to the root node of the tree to traverse
- * @func: a pointer to a function to call for each node
- *
- * Return: void
+ * Return: If tree or func is NULL, do nothing
  */
-
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (tree != NULL &&
-		func != NULL)
-	{
-		binary_tree_inorder(tree->left, func);
-		func(tree->n);
-		binary_tree_inorder(tree->right, func);
-	}
+	if (!tree || !func)
+		return;
+	binary_tree_inorder(tree->left, func);
+	func(tree->n);
+	binary_tree_inorder(tree->right, func);
 }

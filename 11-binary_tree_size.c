62228497f6fad6dfa96203034a4e3f27adca56c7
MariaGeorge22”git config --global user.email maria.george2222@gmail.comgit config --global user.name MariaGeorge22” <maria.george2222@gmail.com>
2024-03-26 11:54:02 -0700

    11-binary_tree_size.c

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,26 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size - binary tree function
+ *
+ * measures the size of a binary tree
+ *
+ * @tree: a pointer to the root node of the tree to measure the size
+ *
+ * Return: size of a binary tree
+ * or 0 If tree is NULL
+ */
+
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t size = 0;
+
+	if (tree != NULL)
+	{
+		size += binary_tree_size(tree->left);
+		size += binary_tree_size(tree->right);
+		size++;
+	}
+
+	return (size);
+}

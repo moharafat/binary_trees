b511d48bacefe3a94719bf42acbf3cdfc003d993
MariaGeorge22”git config --global user.email maria.george2222@gmail.comgit config --global user.name MariaGeorge22” <maria.george2222@gmail.com>
2024-03-26 13:16:17 -0700

    114-bst_remove.c

--- 114-bst_remove.c
+++ 114-bst_remove.c
@@ -1,66 +1,52 @@
+#include <stdlib.h>
 #include "binary_trees.h"
 
+bst_t *inorder_successor(bst_t *node);
+
 /**
- * min_value_node - get the minmum value node
- *
- * Return: pointer to the node
- *
- * @node: the node to return its min value
-*/
-
-bst_t *min_value_node(bst_t *node)
+ * bst_remove - removes a node from a Binary Search Tree
+ * @root: pointer to the root node of the tree where you will remove a node
+ * @value: value to remove in the tree
+ * Return: pointer to the new root node of the tree after removing the desired
+ * value, or NULL if value doesn't exist
+ */
+bst_t *bst_remove(bst_t *root, int value)
 {
-	bst_t *current = node;
+	bst_t *temp = NULL, *successor = NULL;
 
-	while (current && current->left != NULL)
-		current = current->left;
-
-	return (current);
+	if (!root)
+		return (NULL);
+	if (root->n > value)
+		root->left = bst_remove(root->left, value);
+	else if (root->n < value)
+		root->right = bst_remove(root->right, value);
+	else
+	{
+		if (!root->left || !root->right)
+		{
+			temp = root->left ? root->left : root->right;
+			if (temp)
+				temp->parent = root->parent;
+			free(root);
+			return (temp);
+		}
+		successor = inorder_successor(root->right);
+		root->n = successor->n;
+		root->right = bst_remove(root->right, successor->n);
+	}
+	return (root);
 }
 
 /**
- * bst_remove - remove node from bst
- *
- * Return: pointer to root
- *
- * @root: the root of bst
- *
- * @value: the value to remove
-*/
-
-bst_t *bst_remove(bst_t *root, int value)
+ * inorder_successor - finds the inorder successor of a node
+ * @node: pointer to the node to find the successor of
+ * Return: pointer to the inorder successor node of the given node
+ */
+bst_t *inorder_successor(bst_t *node)
 {
-	bst_t *temp;
+	bst_t *current = node;
 
-	if (root == NULL)
-		return (root);
-
-	if (value < root->n)
-		root->left = bst_remove(root->left, value);
-
-	else if (value > root->n)
-		root->right = bst_remove(root->right, value);
-
-	else
-	{
-		if (root->left == NULL)
-		{
-			temp = root->right;
-			free(root);
-			return (temp);
-		}
-		else if (root->right == NULL)
-		{
-			temp = root->left;
-			free(root);
-			return (temp);
-		}
-
-		temp = min_value_node(root->right);
-
-		root->n = temp->n;
-
-		root->right = bst_remove(root->right, temp->n);
-	}
-	return (root);
+	while (current->left)
+		current = current->left;
+	return (current);
 }
